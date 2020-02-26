10250a9c525393d07f37e955d2f041a0beb57e48
Paulo Dias <paulodiasprivado@gmail.com>
2020-02-26 12:10:53 -0300

    Protegendo dados do Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAQY35lnGfpffLZZsr60wfS65w6is7JHO0",
-    authDomain: "surfshop-b12b6.firebaseapp.com",
-    databaseURL: "https://surfshop-b12b6.firebaseio.com",
-    projectId: "surfshop-b12b6",
-    storageBucket: "surfshop-b12b6.appspot.com",
-    messagingSenderId: "89987686576"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
